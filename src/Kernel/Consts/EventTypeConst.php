0df78a9214020d65fd542f8620ef766ea222c827
Nick <i@httpd.cc>
2022-07-14 10:43:30 +0800

    perf: 订单完全退款功能

--- src/Kernel/Consts/EventTypeConst.php
+++ src/Kernel/Consts/EventTypeConst.php
@@ -16,6 +16,7 @@ class EventTypeConst
     public const ORDER_RECEIVE = 'order.receive';
     public const ORDER_DINING = 'order.dining';
     public const ORDER_TAKE_MEAL = 'order.take-meal';
+    public const ORDER_REFUNDED = 'order.refunded';
     public const ORDER_REFUND_AGREE = 'order.refund.agree';
     public const ORDER_REFUND_REFUSE = 'order.refund.refuse';
     public const ORDER_REFUND_CANCEL = 'order.refund.cancel';
@@ -36,6 +37,7 @@ class EventTypeConst
         self::ORDER_RECEIVE => '商户接单',
         self::ORDER_DINING => '商户出餐',
         self::ORDER_TAKE_MEAL => '商户取餐',
+        self::ORDER_REFUNDED => '订单完成退款',
         self::ORDER_REFUND_AGREE => '同意退款',
         self::ORDER_REFUND_REFUSE => '拒绝退款',
         self::ORDER_REFUND_CANCEL => '取消退单',
