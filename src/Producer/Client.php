507ad44d9996f388b883b6bc1a755a2d99e6141b
Nick <i@httpd.cc>
2022-08-19 15:57:52 +0800

    应用迁移

--- src/Producer/Client.php
+++ src/Producer/Client.php
@@ -2,13 +2,16 @@
 
 namespace Cblink\Service\EventCenter\Producer;
 
+use Cblink\Service\Foundation\BaseRequestApi;
+use Closure;
+use InvalidArgumentException;
 use Cblink\Service\EventCenter\Application;
-use Cblink\Service\Foundation\BaseApi;
+use Cblink\Service\EventCenter\Kernel\MessageDto;
 
 /**
  * @property Application $app
  */
-class Client extends BaseApi
+class Client extends BaseRequestApi
 {
     /**
      * 投递消息
@@ -26,9 +29,46 @@ class Client extends BaseApi
 
         $payload = compact('topic', 'type', 'appid', 'data');
 
-        $payload['sign'] = $this->app->buildSign($payload);
+        $payload['sign'] = $this->buildSign($payload);
 
         return $this->httpPost('/producer', $payload);
     }
 
+    /**
+     * 消费消息
+     *
+     * @param Closure $closure
+     * @param array $data
+     * @return mixed
+     * @throws \Throwable
+     */
+    public function consumer(Closure $closure, array $data)
+    {
+        throw_unless($this->verifySign($data), InvalidArgumentException::class, 'Decryption failed.');
+
+        return call_user_func_array($closure, [new MessageDto($data)]);
+    }
+
+    /**
+     * @param array $payload
+     * @return string
+     */
+    public function buildSign(array $payload): string
+    {
+        ksort($payload);
+
+        return hash_hmac('sha1', serialize($payload), md5($this->app->config['public_key']));
+    }
+
+    /**
+     * @param array $payload
+     * @return bool
+     */
+    public function verifySign(array $payload): bool
+    {
+        $sign = $payload['sign'] ?? '';
+        unset($payload['sign']);
+
+        return $this->buildSign($payload) == $sign;
+    }
 }
