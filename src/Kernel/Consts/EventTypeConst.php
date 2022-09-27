a9aec9bf00b422bb44d052886f3ba839ba0b176c
jinjun <757258777@qq.com>
2022-09-27 17:05:22 +0800

    [fix]:加入所有配送订单取消时间

--- src/Kernel/Consts/EventTypeConst.php
+++ src/Kernel/Consts/EventTypeConst.php
@@ -90,6 +90,7 @@ class EventTypeConst
     public const SHOP_DELIVERY_ORDER_STATUS_SUCCESS = 'shop.delivery.order.status.success';
     public const SHOP_DELIVERY_ORDER_STATUS_CANCEL = 'shop.delivery.order.status.cancel';
     public const SHOP_DELIVERY_ORDER_STATUS_EXCEPTION = 'shop.delivery.order.status.exception';
+    public const SHOP_DELIVERY_ORDER_ALL_EXCEPTION = 'shop.delivery.order.all.cancel';
     public const SHOP_DELIVERY = [
         self::ANY => '所有事件',
         self::SHOP_DELIVERY_STATUS => '配送门店审核状态变更',
@@ -99,5 +100,6 @@ class EventTypeConst
         self::SHOP_DELIVERY_ORDER_STATUS_SUCCESS => '门店配送订单已送达',
         self::SHOP_DELIVERY_ORDER_STATUS_CANCEL => '门店配送订单已取消',
         self::SHOP_DELIVERY_ORDER_STATUS_EXCEPTION => '门店配送订单异常',
+        self::SHOP_DELIVERY_ORDER_ALL_EXCEPTION => '门店订单全部取消',
     ];
 }
