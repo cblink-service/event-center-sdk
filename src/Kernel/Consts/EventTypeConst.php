af71d58662398bf86f79d1859a4313ac10df874e
jinjun <757258777@qq.com>
2022-12-01 18:22:40 +0800

    [fix]:用户登陆事件功能

--- src/Kernel/Consts/EventTypeConst.php
+++ src/Kernel/Consts/EventTypeConst.php
@@ -56,12 +56,14 @@ class EventTypeConst
     public const USER_CREATED = 'user.created';
     public const USER_UPDATED = 'user.updated';
     public const USER_ACCOUNT_UPDATED = 'user.account.updated';
+    public const USER_LOGIN = 'user.login';
 
     public const USER = [
         self::ANY => '所有事件',
         self::USER_CREATED => '用户创建',
         self::USER_UPDATED => '用户修改',
         self::USER_ACCOUNT_UPDATED => '用户修改账户',
+        self::USER_LOGIN => '用户登陆'
     ];
 
     // 组织架构
