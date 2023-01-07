65713739026f5f989fa53d2b2fd18b8804307bd2
jinjun <757258777@qq.com>
2023-01-07 15:24:01 +0800

    [fix]:加入商品编辑事件

--- src/Kernel/Consts/EventTypeConst.php
+++ src/Kernel/Consts/EventTypeConst.php
@@ -132,12 +132,15 @@ class EventTypeConst
     ];
 
 
-    public const PRODUCT_SKU_BATCH_UPDATE = 'product.sku.batch-update';
+    public const PRODUCT_CREATE = 'product.create';
     public const PRODUCT_UPDATE = 'product.update';
+    public const PRODUCT_SKU_BATCH_UPDATE = 'product.sku.batch-update';
+    public const PRODUCT_STATUS_BATCH_UPDATE = 'product.status.batch-update';
     public const PRODUCT = [
         self::ANY => '所有事件',
-        self::PRODUCT_SKU_BATCH_UPDATE => '批量更新SKU',
+        self::PRODUCT_CREATE => '商品创建',
         self::PRODUCT_UPDATE => '商品编辑',
-
+        self::PRODUCT_SKU_BATCH_UPDATE => '批量更新SKU',
+        self::PRODUCT_STATUS_BATCH_UPDATE => '批量更新商品状态',
     ];
 }
