86175c8b5dd5e9a090562cc55dd035a991a4acb8
jinjun <757258777@qq.com>
2023-01-11 12:13:37 +0800

    [feat]:加入分销相关事件类型

--- src/Kernel/Consts/EventTypeConst.php
+++ src/Kernel/Consts/EventTypeConst.php
@@ -117,18 +117,22 @@ class EventTypeConst
         self::SHOP_DELIVERY_ORDER_STATUS_EXCEPTION => '门店配送订单异常',
     ];
 
+    public const SALESMAN_CREATE = 'salesman.create';
     public const SALESMAN_WITHDRAWAL_APPLY = 'salesman.withdrawal.apply';
     public const SALESMAN_WITHDRAWAL_PASS = 'salesman.withdrawal.pass';
     public const SALESMAN_WITHDRAWAL_REFUSE = 'salesman.withdrawal.refuse';
     public const SALESMAN_WITHDRAWAL_COMPLETE = 'salesman.withdrawal.complete';
     public const SALESMAN_WITHDRAWAL_FAIL = 'salesman.withdrawal.fail';
+    public const SALESMAN_BALANCE_UPDATE = 'salesman.balance.update';
     public const SALESMAN = [
         self::ANY => '所有事件',
+        self::SALESMAN_CREATE=>'创建分销员',
+        self::SALESMAN_BALANCE_UPDATE =>'分销员余额变更',
         self::SALESMAN_WITHDRAWAL_APPLY => '分销提现-申请',
         self::SALESMAN_WITHDRAWAL_PASS => '分销提现-通过审核',
         self::SALESMAN_WITHDRAWAL_REFUSE => '分销提现-拒绝',
         self::SALESMAN_WITHDRAWAL_COMPLETE => '分销提现-完成',
-        self::SALESMAN_WITHDRAWAL_FAIL => '分销提现-失败'
+        self::SALESMAN_WITHDRAWAL_FAIL => '分销提现-失败',
     ];
 
 
